d7aab16ceefd2c5ee21654a762b56b6cd997c02e
sofiabelber <sofibelber@gmail.com>
2022-11-02 17:15:58 -0300

    implementation of keepers on database

--- DAO/DAODB/KeeperDAODB.php
+++ DAO/DAODB/KeeperDAODB.php
@@ -2,9 +2,42 @@
 	namespace DAO;
 	use Models\Keeper;
 	
-	class KeeperDAO
+	class UserDAO
 	{
+		public function create($keeper){
+			$sql= "INSERT INTO keepers (userId,address_street,address_number,petSize,initialDate,endDate,days,price) VALUES (:userId,:address_street,:address_number,:petSize,:initialDate,:endDate,:days,:price)";
 
+			$parameters['userId']=$keeper->getUserId();
+			$parameters['address_street']=$keeper->getAddressStreet();
+			$parameters['address_number']=$keeper->getAddressNumber();
+			$parameters['petSize']=$keeper->getPetSize();
+			$parameters['initialDate']=$keeper->getInitialDate();
+			$parameters['endDate']=$keeper->getEndDate();
+			$parameters['days']=$keeper->getDays();
+			$parameters['price']=$keeper->getPrice();
+
+			try{
+				$this->connection = Connection::getInstance();
+				return $this->connection->executeNonQuert($sql,$parameters);
+			}catch(\PDOException $ex){
+				throw $ex;
+			}
+		}
+
+		public function addUser($keeper){
+			$Dkeeper = new KeeperDAO;
+			$fileController = new fileController();
+			if($fileController ->upload(){
+				try{
+					$user->create($keeper);
+					return true;
+				}catch(\PDOException $ex){
+					throw $ex;
+				}
+			}else{
+				return false;
+			}
+		}
 	}
 
  ?>
