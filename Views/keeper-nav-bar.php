a34aa9995a4f5bf2d08c47aceca11a8652f70677
AJLapenna <agustinjlapenna@gmail.com>
2022-11-05 01:28:14 -0300

    minor visual fixes

--- Views/keeper-nav-bar.php
+++ Views/keeper-nav-bar.php
@@ -1,8 +1,7 @@
-<h2>PET HERO!</h2>
-
 <nav class="topnav">
     <a href="<?php echo  FRONT_ROOT."Home/Logout "?>">LOGOUT</a><
-    <a href="<?php echo  FRONT_ROOT."Keeper/showHomeView "?>">HOME</a><
     <a href="<?php echo  FRONT_ROOT."User/showMyProfile "?>">SEE MY PERSONAL DATA</a><
     <a href="<?php echo  FRONT_ROOT."Keeper/showKeeperProfile "?>">SEE MY KEEPER PROFILE</a><
+    <a class="active" href="<?php echo  FRONT_ROOT."Keeper/showHomeView "?>">HOME</a><
+    <span style="vertical-align: sub; margin-left: 5px">PET HERO</span>
 </nav> <br>
--- Views/owner-nav-bar.php
+++ Views/owner-nav-bar.php
@@ -1,11 +1,10 @@
-<h2>PET HERO!</h2>
-
 <nav class="topnav">
-    <a href="<?php echo FRONT_ROOT."User/showHomeView "?>">HOME</a>
-    <a href="<?php echo FRONT_ROOT."User/showMyProfile "?>">SEE MY PROFILE</a>
+    <a href="<?php echo FRONT_ROOT."Home/Logout "?>">LOGOUT</a>
+    <a href="<?php echo FRONT_ROOT."Keeper/showListView "?>">FIND A KEEPER !</a>
     <a href="<?php echo FRONT_ROOT."Pet/showAddView "?>">ADD PET</a>
     <a href="<?php echo FRONT_ROOT."Pet/showPetsList "?>">SEE MY PETS</a>
-    <a href="<?php echo FRONT_ROOT."Keeper/showListView "?>">FIND A KEEPER !</a>
-    <a href="<?php echo FRONT_ROOT."Home/Logout "?>">LOGOUT</a>
+    <a href="<?php echo FRONT_ROOT."User/showMyProfile "?>">SEE MY PROFILE</a>
+    <a class="active" href="<?php echo FRONT_ROOT."User/showHomeView "?>">HOME</a>
+    <span style="vertical-align: sub; margin-left: 5px">PET HERO</span>
 </nav> <br>
 
