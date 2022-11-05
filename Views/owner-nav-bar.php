df7574b3966603c22f2d8b8a7b79194dbc9ed635
AJLapenna <agustinjlapenna@gmail.com>
2022-11-05 00:16:38 -0300

    added more styles, labels and structure

--- Views/owner-nav-bar.php
+++ Views/owner-nav-bar.php
@@ -1,16 +1,11 @@
-<h1>PET HERO</h1>
+<h2>PET HERO!</h2>
 
-<nav>
-    <ul>
-        <li>Actions</li>
-        <ul>
-            <li><a href="<?php echo FRONT_ROOT."User/showHomeView "?>">HOME</a></li>
-            <li><a href="<?php echo FRONT_ROOT."User/showMyProfile "?>">SEE MY PROFILE</a></li>
-            <li><a href="<?php echo FRONT_ROOT."Pet/showAddView "?>">ADD PET</a></li>
-            <li><a href="<?php echo FRONT_ROOT."Pet/showPetsList "?>">SEE MY PETS</a></li>
-            <li><a href="<?php echo FRONT_ROOT."Keeper/showListView "?>">FIND A KEEPER !</a></li>
-            <li><a href="<?php echo FRONT_ROOT."Home/Logout "?>">LOGOUT</a></li>
-        </ul>
-    </ul>
-</nav>
+<nav class="topnav">
+    <a href="<?php echo FRONT_ROOT."User/showHomeView "?>">HOME</a>
+    <a href="<?php echo FRONT_ROOT."User/showMyProfile "?>">SEE MY PROFILE</a>
+    <a href="<?php echo FRONT_ROOT."Pet/showAddView "?>">ADD PET</a>
+    <a href="<?php echo FRONT_ROOT."Pet/showPetsList "?>">SEE MY PETS</a>
+    <a href="<?php echo FRONT_ROOT."Keeper/showListView "?>">FIND A KEEPER !</a>
+    <a href="<?php echo FRONT_ROOT."Home/Logout "?>">LOGOUT</a>
+</nav> <br>
 
--- Views/validate-session.php
+++ Views/validate-session.php
@@ -1,3 +1,4 @@
 <?php
   if(!isset($_SESSION["loggedUser"]))
     header("location:../index.php");
+?>
